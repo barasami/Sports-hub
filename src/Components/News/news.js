7b4a25e6b54447a0df2609dc38bf6d7c43b44e42
Barasa O Micheal <46565582+barasami@users.noreply.github.com>
2023-06-18 22:10:01 +0300

    added api

---
+++ src/Components/News/news.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+const Url='https://football98.p.rapidapi.com/premierleague/news'
+
+const options = {
+  headers: {
+    'X-RapidAPI-Key': '87f0c01e35msh8962c83c8aef89ap1e9713jsnbf3fe9f2f818',
+    'X-RapidAPI-Host': 'football98.p.rapidapi.com'
+  }
+};
+
+export const myNews=async()=>{
+    try{
+        const data=await axios.get(Url,options)
+        return data
+    }
+    catch(err){
+        console.log(err);
+    }
+}
