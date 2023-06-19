18b9f950b6e3a189e452dcf8649c0a3766bb86dc
Barasa O Micheal <46565582+barasami@users.noreply.github.com>
2023-06-19 21:44:18 +0300

    updated page

---
+++ src/Components/Transfars/tnews.js
@@ -0,0 +1,24 @@
+const axios = require('axios');
+
+const Url='https://transfermarket.p.rapidapi.com/search'
+
+const options = {
+  params: {
+    query: 'chelsea',
+    domain: 'de'
+  },
+  headers: {
+    'X-RapidAPI-Key': '87f0c01e35msh8962c83c8aef89ap1e9713jsnbf3fe9f2f818',
+    'X-RapidAPI-Host': 'transfermarket.p.rapidapi.com'
+  }
+};
+
+export const nRomours=async()=>{
+    try{
+        const data=await axios.get(Url,options)
+        return data
+    }
+    catch (err){
+        console.log(err);
+    }
+}
