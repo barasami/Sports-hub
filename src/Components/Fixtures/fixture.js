5262a3f04a43bfd07ac19267a857541eabd80ca7
Barasa O Micheal <46565582+barasami@users.noreply.github.com>
2023-06-24 22:13:25 +0300

    fixed data

--- src/Components/Fixtures/fixture.js
+++ src/Components/Fixtures/fixture.js
@@ -1,10 +1,19 @@
 import axios from 'axios';
 
 
+const Url='https://football98.p.rapidapi.com/premierleague/fixtures'
+const options = {
+    headers: {
+      'X-RapidAPI-Key': '87f0c01e35msh8962c83c8aef89ap1e9713jsnbf3fe9f2f818',
+      'X-RapidAPI-Host': 'football98.p.rapidapi.com'
+    }
+};
+
+
 
 export const myFixture=async()=>{
     try{
-        const data=await axios.get()
+        const data=await axios.get(Url,options)
         return data
     }
     catch(err){
