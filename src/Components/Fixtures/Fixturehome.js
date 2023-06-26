6a58840bb543a2da6ea79edee6926745c2057939
Barasa O Micheal <46565582+barasami@users.noreply.github.com>
2023-06-26 09:46:38 +0300

    updated fixtures page

--- src/Components/Fixtures/Fixturehome.js
+++ src/Components/Fixtures/Fixturehome.js
@@ -3,21 +3,42 @@ import { myFixture } from './fixture'
 import './Fixture.css'
 
 function Fixturehome() {
-    const[fixture,setFixture]=useState([])
+  const[fixture,setFixture]=useState([])
     useEffect(()=>{
         myFixture()
         .then(({data})=>{
             setFixture(data)
-            
+            console.log(data);
         })
     },[])
+
+    
+    let myallfix=Object.entries(fixture[0] || {})
+    let allFirst=Object.values(myallfix[0] || {})
+    let allOut=allFirst[1]
+    
+
+    let myAll=allOut?.map(({MatchDay,MatchTime,awayLogo,awayTeam,homeLogo,homeTeam})=>{
+      return(
+        <tr>
+          <td>{MatchDay}</td>
+          <td><img src={homeLogo} alt={homeTeam} /></td>
+          <td>{homeTeam}</td>
+          <td><img src={awayLogo} alt={awayTeam} /></td>
+          <td>{awayTeam}</td>
+          <td>{MatchTime}</td>
+        </tr>
+      )
+    })
 
    
   return (
     <div className='fixture'>
       <h2 className='heading'>Fixture</h2>
       <div>
-
+        <div>
+        {myAll}
+        </div>
       </div>
     </div>
   )
